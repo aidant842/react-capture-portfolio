b042c6a1efa4c15d9249fe5823db300474d9346f
Aidan Thorpe <aidant842@gmail.com>
2021-06-23 11:31:28 +0100

    added nav with styling

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@
 import AboutUs from "./pages/AboutUs";
 //Global Style
 import GlobalStyle from "./components/GlobalStyle";
+
+import Nav from "./components/Nav";
 function App() {
     return (
         <div className="App">
+            <Nav />
             <GlobalStyle />
             <AboutUs />
         </div>
